c55855354ebe59ea9708eb862d2fb39fbb224ea1
Alex <alex.nunn@pm.me>
2023-05-21 13:15:02 -0700

    Implement COMSOL file parser

--- comsol_mesh/parser_mesh.py
+++ comsol_mesh/parser_mesh.py
@@ -1,3 +1,205 @@
 """
 Methods for parsing COMSOL mesh files `.mphtxt`
 """
+import re
+import numpy as np
+
+
+class COMSOLMesh:
+    """Container for mesh objects and vertices"""
+    def __init__(self, header, objects=[]):
+        self.header = header
+        self.objects = []
+
+    def append_object(self, obj):
+        self.objects.append(obj)
+
+    @property
+    def n_objects(self):
+        return len(self.header['types'])
+
+
+class COMSOLParser:
+    """Parser for COMSOL .mphtxt files
+
+    Examples
+    --------
+    >>> path = 'example.mphtxt'
+    >>> comsol_mesh = COMSOLParser.parse(path)
+    """
+
+    def __init__(self, path, stream):
+        self.path = path
+        self.stream = stream
+        self.line_index = 0
+        self.last_line = None
+
+    def parse_header(self):
+        """Parse header of COMSOL .mphtxt file"""
+        stream = self.stream
+        header = dict()
+
+        # Parse version number
+        try:
+            line = self.next_line()
+            v_major, v_minor = map(int, line.split())
+            header['version'] = (v_major, v_minor)
+        except:
+            raise COMSOLParserError('HEADER: Improper version format', self)
+
+        # Parse tags
+        try:
+            line = self.next_line()
+            n_tags = int(re.match(r'(\d+).*', line).group(1))
+            
+            tags = []
+            for i in range(n_tags):
+                line = self.next_line()
+                tag = re.match(r'\d+ (.*)', line).group(1)
+                tags.append(tag)
+            header['tags'] = tags
+        except:
+            raise COMSOLParserError(f'HEADER: Improper tag format on', self)
+        
+        # Parse object types
+        try:
+            line = self.next_line()
+            n_types = int(re.match(r'(\d+).*', line).group(1))
+
+            types = []
+            for i in range(n_types):
+                line = self.next_line()
+                types.append(re.match(r'\d+ (.*)', line).group(1))
+
+            header['types'] = types
+        except:
+            raise COMSOLParserError('HEADER: Improper type format', self)
+        return header
+
+    def parse_int(self):
+        return int(re.match(r'(\d+)( #.*)?', self.next_line()).group(1))
+
+    def parse_object(self):
+        """Return object dict for object definition COMSOL .mphtxt file"""
+        obj_dict = dict()
+
+        # Check header for object
+        line = self.next_line()
+        if line != '0 0 1':
+            raise COMSOLParserError('OBJ HEADER: Improper format', self)
+
+        # Read class type
+        try:
+            obj_dict['class'] = re.match(r'\d+ (\w+) #.*', self.next_line()).group(1)
+            obj_dict['version'] = self.parse_int()
+            obj_dict['sdim'] = sdim = self.parse_int()
+            obj_dict['n_vertices'] = n_vertices = self.parse_int()
+            obj_dict['lowest_vertex_index'] = self.parse_int()
+        except:
+            raise COMSOLParserError('OBJ HEADER: Improper format', self)
+        
+
+        # Read mesh vertex coordinates
+        vertices = np.empty((n_vertices, sdim), dtype=float)
+        for i in range(n_vertices):
+            line = self.next_line()
+            pt = list(map(float, line.split()))
+            if len(pt) != sdim:
+                raise COMSOLParserError('VERTICES : Improper format', self)
+            vertices[i, :] = pt
+        
+        obj_dict['vertices'] = vertices
+
+        # Read number of element types
+        n_types = self.parse_int()
+        types = []
+        
+        for i in range(n_types):
+            type_dict = self.parse_object_type()
+            types.append(type_dict)
+
+        obj_dict['types'] = types
+        
+        return obj_dict
+    
+    def parse_object_type(self):
+        """Return dict for type definition in object from COMSOL file"""
+        type_dict = dict()
+
+        # Type header
+        try:
+            # Read type name
+            line = self.next_line()
+            type_dict['type_name'] = re.match(r'\d+ (\w+) #.*', line).group(1)
+            type_dict['n_vertices_per_element'] = nvp_element = self.parse_int()
+            type_dict['n_elements'] = n_elements = self.parse_int()
+        except:
+            raise COMSOLParserError('TYPE HEADER: Improper format', self)
+
+        # Type element vertex indices
+        try:
+            element_indices = np.empty((n_elements, nvp_element), int)
+            for i in range(n_elements):
+                line = self.next_line()
+                idxs = list(map(int, line.split()))
+                if len(idxs) != nvp_element:
+                    raise COMSOLParserError(
+                        'TYPE ELEMENT INDICES: Incorrect number of indices', self
+                    )
+                element_indices[i, :] = idxs
+
+            type_dict['element_indices'] = element_indices
+        except:
+            raise COMSOLParserError('TYPE ELEMENT INDICES: Improper format', self)
+    
+        # Parse geometric entity indices
+        try:
+            n_geom_entity_idxs = self.parse_int()
+            geom_entity_idxs = []
+            for i in range(n_geom_entity_idxs):
+                geom_entity_idxs.append(self.parse_int())
+            type_dict['geom_entry_idxs'] = geom_entity_idxs
+
+        except:
+            raise COMSOLParserError('TYPE ELEMENT: Geometry entity indices', self)
+        
+        return type_dict
+
+    def next_line(self):
+        """Return next non-blank, non-comment line"""
+        while True:
+            line = self.stream.readline()
+            self.line_index += 1
+
+            if line.startswith('#') or line == '\n':
+                continue
+            elif line == '':
+                raise EOFError()
+            else:
+                line = line.strip()
+                self.last_line = line
+                return line
+
+    @classmethod
+    def parse(cls, path):
+        with open(path) as stream:
+            parser = cls(path, stream)
+            header = parser.parse_header()
+            mesh = COMSOLMesh(header)
+            
+            for i in range(mesh.n_objects):
+                obj = parser.parse_object()
+                mesh.append_object(obj)
+        
+        return mesh
+
+
+class COMSOLParserError(Exception):
+    """Error raised for invalid COMSOL file format"""
+    def __init__(self, comment, parser=None):
+        if parser is not None:
+            message = f'{comment}\n@ line {parser.line_index}: \'{parser.last_line}\''
+        else:
+            message = comment
+
+        super().__init__(message)
---
+++ tests/test_parser_mesh.py
@@ -0,0 +1,163 @@
+"""
+Test COMSOL mesh parser
+"""
+from comsol_mesh.parser_mesh import *
+
+from pprint import pprint
+
+OBJ_TYPES = [
+    {
+        'element_indices': np.array([[0, 5, 4, 1],
+                                [2, 4, 6, 1],
+                                [5, 4, 1, 6],
+                                [3, 5, 1, 6],
+                                [8, 5, 6, 4]]),
+        'geom_entry_idxs': [1, 1, 1, 1, 2],
+        'n_elements': 5,
+        'n_vertices_per_element': 4,
+        'type_name': 'tet'
+    },
+    {
+        'element_indices': np.array([[3, 5, 6, 7, 9, 10],
+                                [8, 6, 5, 11, 10, 9]]),
+        'geom_entry_idxs': [1, 2],
+        'n_elements': 2,
+        'n_vertices_per_element': 6,
+        'type_name': 'prism'
+    },
+    {
+        'element_indices': np.array([[0],
+                                [1],
+                                [2],
+                                [4],
+                                [5],
+                                [6],
+                                [7],
+                                [9],
+                                [10],
+                                [11]]),
+        'geom_entry_idxs': [0, 1, 3, 2, 4, 5, 7, 6, 9, 8],
+        'n_elements': 10,
+        'n_vertices_per_element': 1,
+        'type_name': 'vtx'
+    },
+    {
+        'element_indices': np.array([[ 0,  1],
+                                [ 1,  2],
+                                [ 1,  3],
+                                [ 0,  4],
+                                [ 4,  2],
+                                [ 0,  5],
+                                [ 4,  5],
+                                [ 4,  6],
+                                [ 2,  6],
+                                [ 3,  7],
+                                [ 4,  8],
+                                [ 9,  7],
+                                [ 5,  9],
+                                [ 9, 10],
+                                [ 7, 10],
+                                [ 6, 10],
+                                [ 8, 11],
+                                [11, 10],
+                                [ 9, 11]]),
+        'geom_entry_idxs': [0, 3, 4, 1, 5, 2, 6, 7, 9, 4, 8, 12, 10, 14, 15, 11, 8, 16, 13],
+        'n_elements': 19,
+        'n_vertices_per_element': 2,
+        'type_name': 'edg'
+    },
+    {
+        'element_indices': np.array([[0, 1, 4],
+                                [2, 4, 1],
+                                [0, 5, 1],
+                                [0, 4, 5],
+                                [3, 1, 5],
+                                [2, 6, 4],
+                                [2, 1, 6],
+                                [3, 6, 1],
+                                [5, 6, 4],
+                                [8, 5, 4],
+                                [8, 4, 6],
+                                [7, 9, 10],
+                                [11, 10, 9]]),
+        'geom_entry_idxs': [0, 0, 1, 2, 1, 4, 3, 3, 5, 6, 7, 8, 9],
+        'n_elements': 13,
+        'n_vertices_per_element': 3,
+        'type_name': 'tri'
+    },
+    {
+        'element_indices': np.array([[5, 9, 3, 7],
+                                [3, 7, 6, 10],
+                                [5, 9, 6, 10],
+                                [5, 8, 9, 11],
+                                [8, 6, 11, 10]]),
+        'geom_entry_idxs': [1, 3, 5, 6, 7],
+        'n_elements': 5,
+        'n_vertices_per_element': 4,
+        'type_name': 'quad'
+    }
+]
+
+
+class TestCOMSOLParser:
+    def test_parse_header(self):
+        path = 'tests/data/mesh_example_intro.mphtxt'
+        with open(path) as stream:
+            parser = COMSOLParser(path, stream)
+            header = parser.parse_header()
+            
+            assert set(header.keys()) == set(['version', 'tags', 'types'])
+            assert header['version'] == (0, 1)
+            assert header['tags'] == ['mesh1']
+            assert header['types'] == ['obj']
+
+    def test_parse_object(self):
+        path = 'tests/data/mesh_example_intro.mphtxt'
+        with open(path) as stream:
+            # Read to start of object
+            for i in range(11):
+                stream.readline()
+
+            parser = COMSOLParser(path, stream)
+            obj = parser.parse_object()
+            
+            # Check header & vertices
+            assert set(obj.keys()) == set([
+                    'class', 'version', 'sdim', 'n_vertices', 
+                    'lowest_vertex_index', 'vertices', 'types'
+                    ])
+            assert obj['class'] == 'Mesh'
+            assert obj['version'] == 4
+            assert obj['sdim'] == 3
+            assert obj['n_vertices'] == 12
+            assert obj['lowest_vertex_index'] == 0
+
+            vertices_expect = np.array([
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 1, 1],
+                [1, 0, 1],
+                [0, 1, 0],
+                [1, 0, 0],
+                [1, 1, 1],
+                [2, 0, 1],
+                [1, 1, 0],
+                [2, 0, 0],
+                [2, 1, 1],
+                [2, 1, 0]
+                ], 
+                dtype=float
+            )
+
+            assert np.allclose(obj['vertices'], vertices_expect)
+
+            for type_dict, expect in zip(obj['types'], OBJ_TYPES):
+                for key in type_dict.keys():
+                    if key == 'element_indices':
+                        assert np.allclose(type_dict[key], expect[key])
+                    else:
+                        assert type_dict[key] == expect[key]
+
+    def test_parser(self):
+        path = 'tests/data/mesh_example_intro.mphtxt'
+        obj = COMSOLParser.parse(path)
